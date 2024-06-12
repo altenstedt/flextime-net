74a3d21e3f81a2694f9a7a0a09e5b994720196ad
Martin Altenstedt <Martin.Altenstedt@gmail.com>
2024-06-12 20:10:52 +0200

    Sync command refresh access token when expired
    This patch adds a correct implementation for refreshing the access
    token using the refresh token grant when it is about to expire.

--- Flextime/Constants.cs
+++ Flextime/Constants.cs
@@ -4,4 +4,6 @@ public static class Constants
 {
     public static readonly string MeasurementsFolder =
         $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}/Flextime/measurements";
+
+    public static readonly Uri ApiUri = new Uri("https://api.mangoground-e628dd34.swedencentral.azurecontainerapps.io/", UriKind.Absolute);
 }
