54f75096b1cf694a306570638e34262c28a7668b
Martin Altenstedt <Martin.Altenstedt@gmail.com>
2026-08-13 14:21:41 +0200

    Remove recurring sync status from info

--- Flextime.Daemon/PrintInfo.cs
+++ Flextime.Daemon/PrintInfo.cs
@@ -26,7 +26,6 @@ public class PrintInfo(IHttpClientFactory httpClientFactory, DeviceCode deviceCo
         AnsiConsole.MarkupLine($"Computer id        : {computer.Id}");
         AnsiConsole.MarkupLine($"Time zone          : {PrintTimeZone()}");
         AnsiConsole.MarkupLine($"Listen             : {(SingleInstance.IsHeld(SingleInstance.ListenLockPath) ? "Running" : "Not running")}");
-        AnsiConsole.MarkupLine($"Recurring sync     : {(SingleInstance.IsHeld(SingleInstance.SyncLockPath) ? "Running" : "Not running")}");
         AnsiConsole.MarkupLine($"Server             : {Constants.ApiUri}");
 
         await AnsiConsole.Status()
